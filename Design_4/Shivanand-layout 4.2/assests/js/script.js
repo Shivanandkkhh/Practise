863882f4496beffd59d1686cc0c49432fafd05d8
shivanandkkhh <Shivanand@shoptarde.co>
2022-12-08 10:36:53 +0530

    initial commit

---
+++ Design_4/Shivanand-layout 4.2/assests/js/script.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.submit').click(function() {
+      alert('Thank You for clicking buton 1.')
+    });
+    $('.btn-2').click(function() {
+      alert('Thank You for clicking button 2.')
+    });
+  });
